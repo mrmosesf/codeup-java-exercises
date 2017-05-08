20a86ff5645e5f8245816111a8894b7ac8b6f4fd
Moses Franco <mosesf@outlook.com>
2017-05-08 11:32:42 -0500

    Hello world

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Moses Franco on 5/8/17
+ * Codeup
+ * Pinnacles
+ */
+public class HelloWorld {
+//	psvm
+	public static void main(String[] args) {
+//		sout
+		System.out.println("Hello world!");
+	}
+}
