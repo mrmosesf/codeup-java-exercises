b9526e0d3e875ab531512e6930bf66f38320ad22
Moses Franco <mosesf@outlook.com>
2017-05-08 11:33:02 -0500

    Initial commit

---
+++ src/Types.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Moses Franco on 5/8/17
+ * Codeup
+ * Pinnacles
+ */
+public class Types {
+	public static void main(String[] args) {
+		int myFavoriteNumber = 64;
+		System.out.println(myFavoriteNumber);
+		String myString = "Foo";
+		System.out.println(myString);
+		double myNumber = 3.14;
+		System.out.println(myNumber);
+	}
+}
