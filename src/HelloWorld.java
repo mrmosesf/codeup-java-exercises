ff813842a0aa9c31464ec9ca1bf3e9a92f20cc07
Moses Franco <mosesf@outlook.com>
2017-05-08 11:59:44 -0500

    Added code from Types.java, and completed shorthand exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,5 +8,28 @@ public class HelloWorld {
 	public static void main(String[] args) {
 //		sout
 		System.out.println("Hello world!");
+
+		int myFavoriteNumber = 64;
+		System.out.println(myFavoriteNumber);
+		String myString = "Foo";
+		System.out.println(myString);
+		double myNumber = 3.14;
+		System.out.println(myNumber);
+
+		/*int x = 4;
+		x+= 5;
+		System.out.println(x);
+
+		int x = 3;
+		int y = 4;
+		y *= x;
+		System.out.println(y);
+
+		int x = 10;
+		int y = 2;
+		x /= y;
+		y -= x;
+		System.out.println(x);
+		System.out.println(y);*/
 	}
 }
