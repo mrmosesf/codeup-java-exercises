667321d0fc3fc4f34d49a9122d22fe69061f9cf4
Moses Franco <mosesf@outlook.com>
2017-05-09 10:16:48 -0500

    While loop, and first do-while loop

---
+++ src/Exercises/ControlFlowExercises.java
@@ -0,0 +1,24 @@
+package Exercises;
+
+import java.util.Scanner;
+
+/**
+ * Created by Moses Franco on 5/9/17
+ * Codeup
+ * Pinnacles
+ */
+public class ControlFlowExercises {
+	public static void main(String[] args) {
+		int i =5;
+		while (i<=15){
+			System.out.println(i);
+			i++;
+		}
+
+		int j = 0;
+		do{
+			System.out.println(j);
+			j+=2;
+		}while(j <= 100);
+	}
+}
