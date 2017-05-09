264ca26f29fc4cca38265e2f0c5c22fb685de921
Moses Franco <mosesf@outlook.com>
2017-05-09 15:35:35 -0500

    Completed exercise, where user can give the program a numerical representation of their grade between 100 and 0, and give an alphabet representation, with degree (+/-) based on their input

--- src/Exercises/ControlFlowExercises.java
+++ src/Exercises/ControlFlowExercises.java
@@ -13,14 +13,39 @@ public class ControlFlowExercises {
 
 //		Table of powers
 		Scanner user = new Scanner(System.in);
-
 		PrintStream out = System.out;
-		out.println("What number would you like to go up to?");
-		int userInt = user.nextInt();
-		out.println("Here is your table!\nnumber | squared | cubed\n------ | ------- | -----");
 
-		for (int i = 1; i <= userInt; i++) {
-			out.println(i + "\t   |" + i * i + "\t\t |" + i * i * i);
+		out.println("Enter a grade from 0 to 100: ");
+		int userInt = user.nextInt();
+
+		if (userInt >= 99) {
+			out.println("A+");
+		} else if (98 >= userInt && 94 <= userInt) {
+			out.println("A");
+		} else if (93 >= userInt && 90 <= userInt) {
+			out.println("A-");
+		} else if (89 >= userInt && 86 <= userInt) {
+			out.println("B+");
+		} else if (85 >= userInt && 83 <= userInt) {
+			out.println("B");
+		} else if (82 >= userInt && 80 <= userInt) {
+			out.println("B-");
+		} else if (79 >= userInt && 76 <= userInt) {
+			out.println("C+");
+		} else if (75 >= userInt && 73 <= userInt) {
+			out.println("C");
+		} else if (72 >= userInt && 70 <= userInt) {
+			out.println("C-");
+		} else if (69 >= userInt && 66 <= userInt) {
+			out.println("D");
+		} else if (65 >= userInt && 63 <= userInt) {
+			out.println("D");
+		} else if (62 >= userInt && 60 <= userInt) {
+			out.println("D");
+		} else if (60 >= userInt && 0 <= userInt) {
+			out.println("F");
+		} else {
+			out.println("That's not a grade");
 		}
 	}
 }
