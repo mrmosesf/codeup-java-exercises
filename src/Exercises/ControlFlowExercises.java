d55ff3d1a1a23216f72207149cf459f32a252868
Moses Franco <mosesf@outlook.com>
2017-05-09 11:20:21 -0500

    FizzBuzz exercise with some refactoring

--- src/Exercises/ControlFlowExercises.java
+++ src/Exercises/ControlFlowExercises.java
@@ -1,5 +1,7 @@
 package Exercises;
 
+import java.io.PrintStream;
+
 /**
  * Created by Moses Franco on 5/9/17
  * Codeup
@@ -8,8 +10,22 @@ package Exercises;
 public class ControlFlowExercises {
 	public static void main(String[] args) {
 
-		for (long i =2; i<=1000000; i*=i){
-			System.out.println(i);
+//		Fizzbuzz
+		for(int i=1;i<=100;i++){
+			PrintStream out = System.out;
+			if(i%3 == 0 && i%5 == 0){
+				out.println("FizzBuzz");
+			}
+			else if(i%3 == 0){
+				out.println("Fizz");
+			}
+			else if(i%5 == 0){
+				out.println("Buzz");
+			}
+			else {
+				out.println(i);
+			}
 		}
+
 	}
 }
