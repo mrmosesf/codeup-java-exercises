921fd3f2acbb637be87c068c6960bc007fcfcf52
Moses Franco <mosesf@outlook.com>
2017-05-11 10:07:45 -0500

    Initial commit, with first exercise of completed, rock on

---
+++ src/Exercises/StringExercise.java
@@ -0,0 +1,13 @@
+package Exercises;
+
+/**
+ * Created by Moses Franco on 5/11/17
+ * Codeup
+ * Pinnacles
+ */
+public class StringExercise {
+	public static void main(String[] args) {
+		String message = "We don't need no education\nWe don't need no thought control";
+		System.out.println(message);
+	}
+}
