b6cb2b3271fe6fde3b6aa98acef18799c1f1a79d
Moses Franco <mosesf@outlook.com>
2017-05-12 14:22:03 -0500

    Can now perform factorials, and will be prompted to continue with the function

--- src/Exercises/Methods.java
+++ src/Exercises/Methods.java
@@ -9,6 +9,18 @@ import java.util.Scanner;
  */
 public class Methods {
 
+	public static void calcPrompt(Scanner calculator) {
+		System.out.println("Enter your operator:");
+		String operator = calculator.nextLine();
+		System.out.println("Enter your first value:");
+		int a = calculator.nextInt();
+		System.out.println("Enter your second value:");
+		int b = calculator.nextInt();
+		calcBrain(a, b, operator);
+		System.out.println("Answer is: ");
+
+	}
+
 	public static int addition(int x, int y) {
 		return x + y;
 	}
@@ -68,27 +80,48 @@ public class Methods {
 
 	}
 
-//	public static int getInteger(int min, int max) {
-//		return 1;
-//	}
+	public static int getInteger(int min, int max) {
+		Scanner numberChecked = new Scanner(System.in);
+		int userNumber = numberChecked.nextInt();
+
+		if (min <= userNumber && userNumber <= max) {
+			System.out.println("Yup, that will do");
+			return userNumber;
+		} else {
+			System.out.println("Nope, try again");
+			getInteger(min, max);
+			return 0;
+		}
+	}
+
+	public static long factorialCalc(Scanner calculator) {
+		String wishToContinue;
+		long ans;
+		do {
+			long temp = 1;
+			System.out.println("Enter a number between 1 and 10 to be factored");
+			int j = calculator.nextInt();
+			for (int i = 1; i <= j; i++) {
+				temp *= i;
+			}
+			System.out.println("Answer is: " + temp);
+			ans = temp;
+			System.out.println("Want to continue?");
+			wishToContinue = calculator.next();
+		} while (wishToContinue.equalsIgnoreCase("y"));
+		return ans;
+	}
 
 
 	public static void main(String[] args) {
 		Scanner calculator = new Scanner(System.in);
+//		calcPrompt(calculator);
 
-		System.out.println("Enter your operator:");
-		String operator = calculator.nextLine();
-		System.out.println("Enter your first value:");
-		int a = calculator.nextInt();
-		System.out.println("Enter your second value:");
-		int b = calculator.nextInt();
 
-//		int min = calculator.nextInt();
-//		int max = calculator.nextInt();
+//		System.out.print("Enter a number between 1 and 10: ");
+//		int userInput = getInteger(1, 10);
 
-		System.out.println("Answer is: ");
-		calcBrain(a, b, operator);
-
+		factorialCalc(calculator);
 
 	}
 }
