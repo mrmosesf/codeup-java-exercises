7b5f3c4bf75401b5d785f0579d7c80e8def7e6b2
Moses Franco <mosesf@outlook.com>
2017-05-23 10:26:28 -0500

    Created and completed extra exercise that gives the average for n numbers

---
+++ src/Exercises/Extra/AverageOfN.java
@@ -0,0 +1,27 @@
+package Exercises.Extra;
+
+import java.util.Scanner;
+
+/**
+ * Created by Moses Franco on 5/23/17
+ * Codeup
+ * Pinnacles
+ */
+public class AverageOfN {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		getNAverage(scanner);
+	}
+
+	private static void getNAverage(Scanner scanner) {
+		System.out.println("How many numbers do you want to average?");
+		int numbersNToAverage = scanner.nextInt();
+		double runningAverage = 0;
+
+		for (int i = 1; i <= numbersNToAverage; i++) {
+			System.out.println("Enter number " + i);
+			runningAverage += scanner.nextDouble();
+		}
+		System.out.println("The average value is: " + runningAverage / numbersNToAverage);
+	}
+}
